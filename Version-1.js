884c2bd78ae17756618b18d34774bf868e414d14
Nammaraj <active99raj@gmail.com>
2020-03-03 11:30:40 +1100

    added version 11 and modified logEvents

---
+++ Version-1.js
@@ -0,0 +1,5 @@
+var todos = []; // Creating a place to store my todo items
+console.log('My Todos',todos[0]); // display my first todo item
+todos.push('newTodo'); // Adding a new todo to our todo Array
+todos[0] = 'update with new todoItem' // Updating first todo item
+todos.splice(0,1); // Deleting a first todo item
